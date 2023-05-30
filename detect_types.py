224914ecbef1d839fc9bf4798513936aff013c0c
Rahul Bangre <rahul_bangre@ch.honda.com>
2023-05-30 11:49:26 -0400

    added random sampling

--- detect_types.py
+++ detect_types.py
@@ -1,18 +1,17 @@
 import pandas as pd
+import random
 import sys
 import multiprocessing
 from dateutil.parser import parse
 
 ''' 
-this script takes in two command line arguments, 
-    
-    1. PATH to file 
-    2. threshold 
+this script takes in the path to the source data, and labels the type of each column in the csv.
 
-It then computes if the majority of values in each column are of the same type, and if the
-proportion of values in the column is higher than the threshold, it returns that type for the column. 
+it does this by testing a random sample of rows and seeing if the proportion of enteries in these rows
+for a column is above a certain threshold. If so, the column is deemed to be of that type. Else, it is
+given a type of 'not found'
 
-returns a dict of the type of each column (if determinable). If type is not determinable, then returns 'not found'
+returns a dictionary with the key-value mapping of column-name to type
 
 Example:
     command: py detect_types.py ./example/news_decline.csv 2.9
@@ -24,8 +23,9 @@ STRING = "string"
 INT = "int"
 FLOAT = "float"
 BOOL = "bool"
-THRESHOLD = 0.9
 NOT_FOUND = "not found"
+THRESHOLD = 0.9
+SAMPLE_SIZE = 1000
 
 def is_bool(string):
     string = string.lower()
@@ -35,21 +35,25 @@ def is_bool(string):
 def process_column(column_data):
     column_types = {STRING: 0, INT: 0, FLOAT: 0, DATE: 0, BOOL: 0}
     unique_column_data = set(column_data)
+    column_length = len(column_data)
 
     if len(unique_column_data) == 2 and all(is_bool(value) for value in unique_column_data): return BOOL
     if pd.api.types.is_integer_dtype(column_data): return INT
     if pd.api.types.is_float_dtype(column_data): return FLOAT
 
-    for value in column_data: 
+    sample = range(0, column_length) if column_length <= 1000 else random.sample(range(0, column_length), SAMPLE_SIZE)
+    
+    for i in sample: 
+        value = column_data[i]
         try: 
             parse(value)
             column_types[DATE] += 1
-        except ValueError:
+        except ValueError: 
             column_types[STRING] += 1
 
-    column_length = len(column_data)
-    if float(column_types[DATE] / column_length) >= THRESHOLD: return DATE
-    if float(column_types[STRING] / column_length) >= THRESHOLD: return STRING
+    true_sample_size = len(sample)
+    if float(column_types[DATE] / true_sample_size) >= THRESHOLD: return DATE
+    if float(column_types[STRING] / true_sample_size) >= THRESHOLD: return STRING
     return NOT_FOUND
 
 def get_types(data_path):
